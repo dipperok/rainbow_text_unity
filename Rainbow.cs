ebbed1a1a77d27ffa14c38b7190a8d58b7c58678
dipperok <57329478+dipperok@users.noreply.github.com>
2022-06-26 00:38:24 +0300

    Update Rainbow.cs

--- Rainbow.cs
+++ Rainbow.cs
@@ -14,14 +14,11 @@ public class Rainbow : MonoBehaviour
     void Start()
     {
         rainbowSpeed = 0.05f;
-
         colorR = 1f;
         colorG = 1f;
         colorB = 0f;
-
         textForRainbow.color = new Color(colorR, colorG, colorB);
         StartCoroutine(RainbowCoroutine());
-
     }
     IEnumerator RainbowCoroutine()
     {
@@ -31,64 +28,47 @@ public class Rainbow : MonoBehaviour
             { 
                 textForRainbow.color = new Color(textForRainbow.color.r, textForRainbow.color.g - 0.1f, textForRainbow.color.b);
                 if (textForRainbow.color.g < 0f)
-                {
                     textForRainbow.color = new Color(textForRainbow.color.r, 0f, textForRainbow.color.b);
-                }
             }
             if (textForRainbow.color.g == 0f)
             { 
                 textForRainbow.color = new Color(textForRainbow.color.r, textForRainbow.color.g, textForRainbow.color.b + 0.1f);
                 if (textForRainbow.color.b > 1f)
-                {
                     textForRainbow.color = new Color(textForRainbow.color.r, textForRainbow.color.g, 1f);
-                }
             }
         }
-        
         if (textForRainbow.color.b == 1f)
         {
             if (textForRainbow.color.g == 0f)
             { 
                 textForRainbow.color = new Color(textForRainbow.color.r - 0.1f, textForRainbow.color.g, textForRainbow.color.b);
                 if (textForRainbow.color.r < 0f)
-                {
                     textForRainbow.color = new Color(0f, textForRainbow.color.g, textForRainbow.color.b);
-                }
             }
             if (textForRainbow.color.r == 0f)
             { 
                 textForRainbow.color = new Color(textForRainbow.color.r, textForRainbow.color.g + 0.1f, textForRainbow.color.b);
                 if (textForRainbow.color.g > 1f)
-                {
                     textForRainbow.color = new Color(textForRainbow.color.r, 1f, textForRainbow.color.b);
-                }
                 if (textForRainbow.color.g < 0f)
-                {
                     textForRainbow.color = new Color(textForRainbow.color.r, 0f, textForRainbow.color.b);
-                }
             }
         }
-        
         if (textForRainbow.color.g == 1f)
         {
             if (textForRainbow.color.r == 0f)
             { 
                 textForRainbow.color = new Color(textForRainbow.color.r, textForRainbow.color.g, textForRainbow.color.b - 0.1f);
                 if (textForRainbow.color.b < 0f)
-                {
                     textForRainbow.color = new Color(textForRainbow.color.r, textForRainbow.color.g, 0f);
-                }
             }
             if (textForRainbow.color.b == 0f)
             { 
                 textForRainbow.color = new Color(textForRainbow.color.r + 0.1f, textForRainbow.color.g, textForRainbow.color.b);
                 if (textForRainbow.color.r > 1f)
-                {
                     textForRainbow.color = new Color(1f, textForRainbow.color.g, textForRainbow.color.b);
-                }
             }
         }
-
         yield return new WaitForSeconds(rainbowSpeed);
         StartCoroutine(RainbowCoroutine());
     }
